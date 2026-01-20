202cc0c0a689849db1b8ab7b47ffd31ff121ad99
omprakashsaggam <omprakash.saggam@gmail.com>
2026-01-20 13:47:51 +0100

    initial commit

---
+++ src/main/java/com/medibridge/apigateway_service_medibridge/ApigatewayServiceMedibridgeApplication.java
@@ -0,0 +1,13 @@
+package com.medibridge.apigateway_service_medibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServiceMedibridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServiceMedibridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medibridge/apigateway_service_medibridge/ApigatewayServiceMedibridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibridge.apigateway_service_medibridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceMedibridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
