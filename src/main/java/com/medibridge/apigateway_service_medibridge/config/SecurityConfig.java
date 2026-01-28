124c38476392c88a1ff834a366c72d1a4fd80c3d
omprakashsaggam <omprakash.saggam@gmail.com>
2026-01-28 22:33:24 +0100

    prod setup

--- src/main/java/com/medibridge/apigateway_service_medibridge/config/SecurityConfig.java
+++ src/main/java/com/medibridge/apigateway_service_medibridge/config/SecurityConfig.java
@@ -222,7 +222,7 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:3000"));
+                config.setAllowedOrigins(List.of("http://localhost:3000","https://medibridge-prod.vercel.app/"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                 config.setExposedHeaders(List.of("Authorization", "Content-Disposition", "Content-Type"));
                 config.setAllowCredentials(true);
